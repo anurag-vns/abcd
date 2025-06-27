c72ea4e8d52f5aa5a974e4599abd5d52d077b7ea
anurag-vns <129647178+anurag-vns@users.noreply.github.com>
2025-06-27 18:07:40 +0530

    updated

--- src/About.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-function About() {
-  return <h2>About Us Page</h2>;
-}
-export default About;
--- src/About1.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function About(){
-    return<h2>Welcome to about</h2>
-}
-
-export default About;
